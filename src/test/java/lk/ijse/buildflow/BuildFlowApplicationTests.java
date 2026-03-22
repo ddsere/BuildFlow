ed5805ed9ca9e0651380df9ad467db269416e8a6
Daham <157113991+ddsere@users.noreply.github.com>
2026-03-22 13:10:35 +0530

    project structure

---
+++ src/test/java/lk/ijse/buildflow/BuildFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.buildflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
